1b0439a3593508b225db51755f3f290a1b588b7f
Your Name <you@example.com>
2024-04-13 22:08:11 -0300

    update

--- installProcess.sh
+++ installProcess.sh
@@ -1,6 +1,16 @@
+echo
+echo -n "Cargando... |"
+echo
+npm install & pid1=$!
+npx playwright install & pid2=$!
+
+# Mostrar la barra de carga mientras se ejecutan npm install y npx playwright install
+while kill -0 $pid1 &>/dev/null || kill -0 $pid2 &>/dev/null; do
+    echo -n "■"
+    sleep 1  # Ajustar la velocidad de la barra de carga según sea necesario
+done
+
+# Mostrar carga completa cuando las instalaciones hayan terminado
+echo
+echo -e " Carga completada.\n"
 cat starship.txt
-echo
-echo
-npm install
-echo
-npx playwright install

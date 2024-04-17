02d858d001a349276d66244d541d372e909048d5
Your Name <you@example.com>
2024-04-17 10:24:05 -0300

    update steps/features

--- src/test/features/Practice.feature
+++ src/test/features/Practice.feature
@@ -8,38 +8,38 @@ Given El usuario completa el campo username con "student" .
 When El usuario hace click "Test login" boton .
 And Completa el  campo password con "Password123" .
 And Hace click en "Submit"
-Then Se muestra el mensaje "Logged In Successfully" .
-When El usuario hace click en "Practice" boton .
-When El usuario hace click en el test "Test Exceptions"
-When usuario hace click en "Add"
-And Verifica que "Row 2" se desplegue
-And completar "Row 2 " con "add"
+# Then Se muestra el mensaje "Logged In Successfully" .
+# When El usuario hace click en "Practice" boton .
+# When El usuario hace click en el test "Test Exceptions"
+# When usuario hace click en "Add"
+# And Verifica que "Row 2" se desplegue
+# And completar "Row 2 " con "add"
 
-Scenario: Test error
-Given El usuario abre la url .
-When El usuario hace click en "Practice" boton .
-When El usuario hace click "Test login" boton .
-Given El usuario completa el campo con "student22" .
-And Completa el  campo con "Password12322" .
-And Hace click en "Submit"
-Then Se muestra el mensaje "Logged In Successfully" .
+# Scenario: Test error
+# Given El usuario abre la url .
+# When El usuario hace click en "Practice" boton .
+# When El usuario hace click "Test login" boton .
+# Given El usuario completa el campo con "student22" .
+# And Completa el  campo con "Password12322" .
+# And Hace click en "Submit"
+# Then Se muestra el mensaje "Logged In Successfully" .
 
-Scenario: test error username
-Given El usuario abre la url .
-When El usuario hace click en "Practice" boton .
-When El usuario hace click "Test login" boton .
-And El usuario escribe "incorrectUser" en e campo "username" 
-And Completa el  campo con "Password12322" .
-And Hace click en "Submit"
-Then Se muestra el error "Your username is invalid!"
+# Scenario: test error username
+# Given El usuario abre la url .
+# When El usuario hace click en "Practice" boton .
+# When El usuario hace click "Test login" boton .
+# And El usuario escribe "incorrectUser" en e campo "username" 
+# And Completa el  campo con "Password12322" .
+# And Hace click en "Submit"
+# Then Se muestra el error "Your username is invalid!"
 
-Scenario: test error username
-Given El usuario abre la url .
-When El usuario hace click en "Practice" boton .
-When El usuario hace click "Test login" boton .
-Given El usuario completa el campo con "student22" .
-And El usuario escribe con "incorrectPassword" en e campo "username" 
-And Hace click en "Submit"
-Then Se muestra  "Your password is invalid!"
+# Scenario: test error username
+# Given El usuario abre la url .
+# When El usuario hace click en "Practice" boton .
+# When El usuario hace click "Test login" boton .
+# Given El usuario completa el campo con "student22" .
+# And El usuario escribe con "incorrectPassword" en e campo "username" 
+# And Hace click en "Submit"
+# Then Se muestra  "Your password is invalid!"
 
  
--- src/test/steps/PracticeSteps.ts
+++ src/test/steps/PracticeSteps.ts
@@ -43,90 +43,90 @@ Given('El usuario abre la url .', async function () {
     const testButton = await pageFixture.page.locator('//*[@id="passwordNext"]/div/button/span');
     await pageFixture.page.waitForTimeout(1000);
     await testButton.click();
-    await pageFixture.page.waitForTimeout(15000); // Esperar un breve tiempo para asegurar que la página se cargue completamente
+    await pageFixture.page.waitForTimeout(18000); // Esperar un breve tiempo para asegurar que la página se cargue completamente
     await pageFixture.page.screenshot({ path: 'captura.png' }); // Guardar la captura de pantalla
     
     // await pageFixture.page.waitForTimeout(10000);
   })
 
-  Then('Se muestra el mensaje {string} .', async function (string) {
-    await pageFixture.page.locator('//*[@id="loop-container"]/div/article/div[1]/h1');
-    await pageFixture.page.waitForTimeout(2000);
+//   Then('Se muestra el mensaje {string} .', async function (string) {
+//     await pageFixture.page.locator('//*[@id="loop-container"]/div/article/div[1]/h1');
+//     await pageFixture.page.waitForTimeout(2000);
 
-  })
+//   })
   
-  When('El usuario hace click en el test {string}', async (s: string) => {
-    const testexButton = await pageFixture.page.locator('//*[@id="loop-container"]/div/article/div[2]/div[2]/div[1]/p/a');
-    await pageFixture.page.waitForTimeout(2000);
-    await testexButton.click();
+//   When('El usuario hace click en el test {string}', async (s: string) => {
+//     const testexButton = await pageFixture.page.locator('//*[@id="loop-container"]/div/article/div[2]/div[2]/div[1]/p/a');
+//     await pageFixture.page.waitForTimeout(2000);
+//     await testexButton.click();
 
-  })
+//   })
  
  
-  When('usuario hace click en {string}', async (s: string) => {
-  const addButton = await pageFixture.page.locator('//*[@id="add_btn"]');
-  await pageFixture.page.evaluate(() => {
-    const scrollPosition = 200; 
-    document.documentElement.scrollTo(0, scrollPosition);
-  })
-     await pageFixture.page.waitForTimeout(1000); 
+//   When('usuario hace click en {string}', async (s: string) => {
+//   const addButton = await pageFixture.page.locator('//*[@id="add_btn"]');
+//   await pageFixture.page.evaluate(() => {
+//     const scrollPosition = 200; 
+//     document.documentElement.scrollTo(0, scrollPosition);
+//   })
+//      await pageFixture.page.waitForTimeout(1000); 
 
-   await addButton.click();
-  })
+//    await addButton.click();
+//   })
 
-  Then('Verifica que {string} se desplegue', async (s: string) => {
-    await pageFixture.page.locator('//*[@id="row2"]/input');
-    await pageFixture.page.waitForTimeout(4000);
-  })
+//   Then('Verifica que {string} se desplegue', async (s: string) => {
+//     await pageFixture.page.locator('//*[@id="row2"]/input');
+//     await pageFixture.page.waitForTimeout(4000);
+//   })
 
   
-  When('completar {string} con {string}', async (s: string, s2: string) => {
-    const rowInput = await pageFixture.page.locator('//*[@id="row2"]/input');
-    await rowInput.fill('add');
-    await pageFixture.page.waitForTimeout(2000);
+//   When('completar {string} con {string}', async (s: string, s2: string) => {
+//     const rowInput = await pageFixture.page.locator('//*[@id="row2"]/input');
+//     await rowInput.fill('add');
+//     await pageFixture.page.waitForTimeout(2000);
   
-  })
+//   })
 
-  Given('El usuario completa el campo con {string} .', async (s: string) => {
-    const usernameInput = await pageFixture.page.locator('//*[@id="username"]');
-    await pageFixture.page.evaluate(() => {
-      const scrollPosition = 200; // Ajusta la posición de desplazamiento según sea necesario
-      document.documentElement.scrollTo(0, scrollPosition);
-    })
+//   Given('El usuario completa el campo con {string} .', async (s: string) => {
+//     const usernameInput = await pageFixture.page.locator('//*[@id="username"]');
+//     await pageFixture.page.evaluate(() => {
+//       const scrollPosition = 200; // Ajusta la posición de desplazamiento según sea necesario
+//       document.documentElement.scrollTo(0, scrollPosition);
+//     })
         
-    await usernameInput.fill('student');
+//     await usernameInput.fill('student');
       
-  })
-  Given('Completa el  campo con {string} .', async (s: string) => {
-    const usernameInput = await pageFixture.page.locator('//*[@id="password"]');
-    await usernameInput.fill('Password12322');
-  })
+//   })
+//   Given('Completa el  campo con {string} .', async (s: string) => {
+//     const usernameInput = await pageFixture.page.locator('//*[@id="password"]');
+//     await usernameInput.fill('Password12322');
+//   })
 
   
-  When('El usuario escribe {string} en e campo {string}', async (s: string, s2: string) => {
-    const usernameInput = await pageFixture.page.locator('//*[@id="username"]');
-    await pageFixture.page.evaluate(() => {
-      const scrollPosition = 200; // Ajusta la posición de desplazamiento según sea necesario
-      document.documentElement.scrollTo(0, scrollPosition);
-    })
+//   When('El usuario escribe {string} en e campo {string}', async (s: string, s2: string) => {
+//     const usernameInput = await pageFixture.page.locator('//*[@id="username"]');
+//     await pageFixture.page.evaluate(() => {
+//       const scrollPosition = 200; // Ajusta la posición de desplazamiento según sea necesario
+//       document.documentElement.scrollTo(0, scrollPosition);
+//     })
         
-    await usernameInput.fill('incorrectUser');
+//     await usernameInput.fill('incorrectUser');
       
-  })
+//   })
   
-  Then('Se muestra el error {string}', async (s: string) => {
-    await pageFixture.page.locator('//*[@id="error"]');
-    await pageFixture.page.waitForTimeout(2000);
-  })
+//   Then('Se muestra el error {string}', async (s: string) => {
+//     await pageFixture.page.locator('//*[@id="error"]');
+//     await pageFixture.page.waitForTimeout(2000);
+//   })
 
-  Given('El usuario escribe con {string} en e campo {string}', async (s: string, s2: string) => {
-    const passwordInput = await pageFixture.page.locator('//*[@id="password"]');
-    await passwordInput.fill(' incorrectPassword');
+//   Given('El usuario escribe con {string} en e campo {string}', async (s: string, s2: string) => {
+//     const passwordInput = await pageFixture.page.locator('//*[@id="password"]');
+//     await passwordInput.fill(' incorrectPassword');
   
-  })
+//   })
 
-Then('Se muestra  {string}', async (s: string) => {
-  await pageFixture.page.locator('//*[@id="error"]');
-  await pageFixture.page.waitForTimeout(2000);
-})
+// Then('Se muestra  {string}', async (s: string) => {
+//   await pageFixture.page.locator('//*[@id="error"]');
+//   await pageFixture.page.waitForTimeout(2000);
+// })
 

cdb6d6307eaec96e201e3d8ca3f3776e5edfe178
Thiago <149462281+thiagotobiasturk@users.noreply.github.com>
2024-04-18 02:41:45 -0300

    Add files via upload

---
+++ upload.sh
@@ -0,0 +1,7 @@
+# #!/bin/bash
+# TOKEN="xoxp-6209710763493-6236228023216-6979539811362-e7d64f3e049afb34ab7dc719d4bfd020"
+# PHOTO_FILE="captura.png"
+# CHANNEL_ID="C06S26FBWP2"  
+
+# curl -F "token=$TOKEN" -F "channels=$CHANNEL_ID" -F "file=@$PHOTO_FILE" https://slack.com/api/files.upload
+
---
+++ upload2.sh
@@ -0,0 +1,6 @@
+# #!/bin/bash
+# TOKEN="xoxp-6209710763493-6236228023216-6979539811362-e7d64f3e049afb34ab7dc719d4bfd020"
+# ZIP_FILE="index.html"
+# CHANNEL_ID="C06S26FBWP2"
+
+# curl -F "token=$TOKEN" -F "channels=$CHANNEL_ID" -F "file=@$ZIP_FILE" https://slack.com/api/files.upload

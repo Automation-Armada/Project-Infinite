62567bdc34e225d3dcf5620a797929d754f0300e
Thiago <149462281+thiagotobiasturk@users.noreply.github.com>
2024-04-18 12:04:08 -0300

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TOKEN="SLACK_TOKEN"
-PHOTO_FILE="captura.png"
+PHOTO_FILE="/captura.zip"
 CHANNEL_ID="C06S26FBWP2"  
 
 curl -F "token=$TOKEN" -F "channels=$CHANNEL_ID" -F "file=@$PHOTO_FILE" https://slack.com/api/files.upload

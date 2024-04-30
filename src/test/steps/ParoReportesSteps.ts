c924f6cfe7c8cc74a80abb8370e8ce0588d69916
Thiago <149462281+thiagotobiasturk@users.noreply.github.com>
2024-04-30 13:15:53 -0300

    Update ParoReportesSteps.ts

--- src/test/steps/ParoReportesSteps.ts
+++ src/test/steps/ParoReportesSteps.ts
@@ -19,17 +19,17 @@ Given('El usuario abre la pagina {string}', async function (string) {
     const iconButton = await pageFixture.page.locator('//*[@id="jZ2SBf"]/div[1]/span');
     await iconButton.click();
    
-  const currentURL = pageFixture.page.url();
+  // const currentURL = pageFixture.page.url();
 
   
-  const csvWriter = createObjectCsvWriter({
-    path: 'current_link.csv',
-    header: [
-      { id: 'url', title: 'Enlace' }
-    ]
-  });
-  await csvWriter.writeRecords([{ url: currentURL }]);
-  console.log('Enlace actual guardado en current_link.csv en la raíz del repositorio.');
+  // const csvWriter = createObjectCsvWriter({
+  //   path: 'current_link.csv',
+  //   header: [
+  //     { id: 'url', title: 'Enlace' }
+  //   ]
+  // });
+  // await csvWriter.writeRecords([{ url: currentURL }]);
+  // console.log('Enlace actual guardado en current_link.csv en la raíz del repositorio.');
     await pageFixture.page.waitForTimeout(5000);
     await pageFixture.page.screenshot({ path: 'paro.png'});
   });

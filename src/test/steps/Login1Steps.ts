49b1305d29db90b11fc21b10119e5741189c6d8c
Your Name <you@example.com>
2024-05-30 16:49:04 -0300

    dsdsds

--- src/test/steps/Login1Steps.ts
+++ src/test/steps/Login1Steps.ts
@@ -2,25 +2,26 @@ import { Given, When } from "@cucumber/cucumber";
 import { pageFixture } from "../../hooks/pageFixture";
 
 
-Given('sdnsdnsndinsnis', async function () {
-    await pageFixture.page.goto("https://login.lirmipre.com/login"); 
+Given('El usuario se logea en {string}', async (s: string) => {
+  await pageFixture.page.goto("https://login.lirmipre.com/login"); 
 });
 
 
-  Given('sdsdmskdmksdmk', async function () {
+    Given('El usuario completa el campo Gamail con {string}', async (s: string) => {
     const usernameInput = await pageFixture.page.locator('//*[@id="email"]');
     await usernameInput.fill('alejandro.gamboa@lirmi.com');
-        
+    
   });
-
-
-  Given('kndfkdfnkdnfk', async function () {
+  
+  
+  Given('El usuario completa el campO Password con {string}', async (s: string) => {
     const usernameInput = await pageFixture.page.locator('//*[@id="password"]');
     await usernameInput.fill('secret');
     });
 
 
-  When('sdsdsdsdsdsds', async function () {
+  When('eL uSuario hace click en {string} button', async (s: string) => {
     const practiceButton = await pageFixture.page.locator('//*[@id="submit"]');
     await practiceButton.click();
   });
+
